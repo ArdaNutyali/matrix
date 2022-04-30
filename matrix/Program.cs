0f35f5123b0076ea7209a4c886f4935eb7a6b671
ArdaNutyali <64331573+ArdaNutyali@users.noreply.github.com>
2022-05-01 01:45:20 +0300

    Proje dosyası ekle.

---
+++ matrix/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[,] dizi = new int[5, 3];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    dizi[i, j] = rnd.Next(1, 100);
+                    Console.Write(dizi[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
